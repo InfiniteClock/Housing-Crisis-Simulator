93226298fa350c8af7e677ccc6d1ccdbcb97516c
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2026-03-08 13:33:01 -0400

    prefab

---
+++ Assets/Scripts/Landlord Script/LandlordSetup.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LandlordSetup : MonoBehaviour
+{
+    public Sprite landlordSprite;
+    public string name;
+    public int companyInt;
+    public int Trait01;
+    public int Trait02;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Image thisSprite = GetComponent<Image>();
+        thisSprite.sprite = landlordSprite;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
