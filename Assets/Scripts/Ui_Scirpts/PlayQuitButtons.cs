ce4f2c0060a9300a2cc974f22381a382ce124cae
sin-sord <148483596+sin-sord@users.noreply.github.com>
2026-03-18 19:32:29 -0400

    buttons now function appropriately
    - Return To Menu button resets the game and returns player to menu
    - Quit button ends the run and quits application
    - Construct button takes player to the game

--- Assets/Scripts/Ui_Scirpts/PlayQuitButtons.cs
+++ Assets/Scripts/Ui_Scirpts/PlayQuitButtons.cs
@@ -8,17 +8,34 @@ public class PlayGameScript : MonoBehaviour
    
     public void PlayGame()
     {
-        SceneManager.LoadScene("System Setup");
+        //     SceneManager.LoadScene("System Setup");
+
+        // If player presses "Construct" button then the scene changes to the game
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) %
+            SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
     }
 
     public void ReturnToMenu()
     {
-        SceneManager.LoadScene("Main Menu");
+   //     SceneManager.LoadScene("Main Menu");
+
+        // If player presses "Return To Menu" button then the scene changes to the main menu scene and resets the game
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex - 1) %
+            SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
     }
 
     public void QuitGame()
     {
+        // If the "QUIT" button is pressed then the application closes, and the run in-engine ends.
+       #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
         Application.Quit();
+
     }
 
 }
